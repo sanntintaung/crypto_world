9821fd9fd20e2ea1374b75fc498f1564fa7dfe09
sanntintaung <sanntintaung009@gmail.com>
2021-09-27 19:33:15 +0630

    Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import millify from "millify";
+import { Row ,Col, Statistic, Typography } from "antd";
 
+
+const {Title} = Typography;
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <React.Fragment>
+            <Title level={2} className="heading">
+                Global Crypto Stats
+            </Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value="5" /></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+            </Row>
+        </React.Fragment>
     );
 };
 
